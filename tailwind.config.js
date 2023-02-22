e022c968683107fe67dbc6e3cb752e6db840f245
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-02-22 12:30:56 -0500

    Added home page content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./index.html', './src/**/*.{html, js}'],
   theme: {
     extend: {},
+    fontFamily: {
+      Inter: ['Inter', 'sans-serif'],
+      Babylonica: ['Babylonica', 'cursive']
+    }
   },
   plugins: [],
 }
