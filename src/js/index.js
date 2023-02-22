9c4b0b8128f8e1a02e6dfa15258021d95220eecd
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-02-22 15:58:02 -0500

    Added error message function

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,14 @@ const nav__logo = document.querySelector("#nav--logo");
 const home__content = document.querySelector('#home--content');
 const pokemon__content = document.querySelector("#pokemon--content");
 const pokemon__input = document.querySelector("#pname-input");
+const pokemon__species = document.querySelector("#pokemon--species");
+const pokemon__type = document.querySelector("#pokemon--type");
+const pokemon__image = document.querySelector("#pokemon--image");
+
+// Initial page DOM element styles
 pokemon__content.style.display = 'none';
+
+const url = 'https://pokeapi.co/api/v2/pokemon/';
 
 nav__logo.addEventListener('click', (event) => {
     home__content.style.removeProperty('display');
@@ -14,5 +21,32 @@ search__button.addEventListener('click', (event) => {
     home__content.style.display = 'none';
     pokemon__content.style.removeProperty('display');
 
+    fetch(url + pokemon__input.value.toLowerCase()).then((response) => {
+        console.log('resolved', response);
+        if (response.status == 404) {
+            displayHandler__err();
+        } else {
+            return response.json();
+        }
+    }).then((data) => {
+        displayHandler(data);
+    }).catch((err) => {
+        console.log('Network Error:', err);
+    });
+
     pokemon__input.select();
 });
+const displayHandler = function (data) {
+    console.log(data);
+
+    // Data Retrieval
+    pokemon__species.innerText = data.species.name;
+
+    pokemon__image.setAttribute('src', data.sprites.front_default);
+    // Data Styling
+}
+const displayHandler__err = function () {
+    pokemon__species.innerText = "Sorry, we could not find that Pokémon";
+    pokemon__type.innerText = '';
+    pokemon__image.setAttribute('src', './src/assets/images/sad-pokemon.png');
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{html, js}'],
   theme: {
-    extend: {},
-    fontFamily: {
-      Inter: ['Inter', 'sans-serif'],
-      Babylonica: ['Babylonica', 'cursive']
-    }
+    extend: {
+      colors: {
+        'normal': '#A8A878',
+        'fire': '#F08030',
+        'water': '#6890F0',
+        'grass': '#78C850',
+        'electric': '#F8D030',
+        'ice': '#98D8D8',
+        'fighting': '#C03028',
+        'poison': '#A040A0',
+        'ground': '#E0C068',
+        'flying': '#A890F0',
+        'psychic': '#F85888',
+        'bug': '#A8B820',
+        'rock': '#B8A038',
+        'ghost': '#705898',
+        'dark': '#705848',
+        'dragon': '#7038F8',
+        'steel': '#B8B8D0',
+        'fairy': '#EE99AC'
+      },
+      fontFamily: {
+        Inter: ['Inter', 'sans-serif'],
+        Babylonica: ['Babylonica', 'cursive'],
+        Phudu: ['Phudu', 'cursive']
+      },
+    },
+
   },
   plugins: [],
 }
